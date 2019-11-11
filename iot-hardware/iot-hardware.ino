8786146fe4f9589790390c1a7a9bab01ee50581c
Jason Rabely <rabelyj@gmail.com>
2019-11-11 17:19:40 -0600

    UPDATED: new version.

--- iot-hardware/iot-hardware.ino
+++ iot-hardware/iot-hardware.ino
@@ -1,3 +1,5 @@
+
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
@@ -5,21 +7,43 @@
 #include <Wire.h>
 
 // Network constants
-#define TEAM_NAME "01"
+#define TEAM_NAME "node"
 int PUBLISH_PERIOD = 1000;
 
-int id = 01;
+char json[] = "{\"id\":\"id01\",\"type\":\"input\",\"sensor\":\"323\"}"; 
+
+
+//IDS Generators
+long randomId;
+char* randomChar[40] = {"a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+String idDispositivo = "id00";
+String idOutput1 = "id02";
+String idOutput2 = "id03";
+String idInput = "id01";
+
+//Spected Message
+
+//String example = "{\"id\":\"12y\",\"type\":\"input\",\"sensor\":\"400\"}";
+// id01,ON,true;
+// 1i,0,1000;
+
+//==============
 
 const char* ssid = "Nexxt_36F1B0";
 const char* password = "";
-const char* mqtt_server = "192.168.0.102";
-
-//const char* password = "f4EF0B4bc2";
-//const char* mqtt_server = "192.168.1.22";
+const char* mqtt_server = "192.168.0.111";
 
 // I/O constants
 #define TEMP_PIN A0
 #define LED_PIN D5
+#define BOMB_PIN D6
+
+
+const char openB = '{';
+const char closeB = '}';
+
+char *token,*ok1,*ok2;
 
 int red=0;
 int blue = 0;
@@ -41,16 +65,16 @@ int lastButtonState = 0;  //
 int lastReadingTime = 0;
 double temp = 0;
 
-char msg[50];
+char msg[150];
 char cnt[50];
-char msg_r[50];
+char msg_r[100];
 char topic_name[250];
 
 // network variables
 WiFiClient espClient;
 PubSubClient mqtt_client(espClient);
 
-
+  
 // temp
 BME280I2C::Settings mysettings; 
 //(
@@ -67,6 +91,119 @@ BME280I2C::Settings mysettings;
 BME280I2C bme(mysettings);    // Default : forced mode, standby time = 1000 ms
                   // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
 
+
+
+void generateIdDispositivo(){
+  String str(random(10, 90));
+  String str2(randomChar[random(0, 25)]);
+  
+  idDispositivo = str + str2;
+  Serial.print("ID DISPOSITIVO: ");
+  Serial.println(idDispositivo);
+}
+
+
+void generateIdOutput1(){
+  String str(random(10, 90));
+  String str2(randomChar[random(0, 25)]);
+  
+  idOutput1 = str + str2;
+  Serial.print("ID Output1: ");
+  Serial.println(idOutput1);
+}
+
+void generateIdOutput2(){
+  String str(random(10, 90));
+  String str2(randomChar[random(0, 25)]);
+  
+  idOutput2 = str + str2;
+  Serial.print("ID Output2: ");
+  Serial.println(idOutput2);
+}
+
+void generateIdInput(){
+  String str(random(10, 90));
+  String str2(randomChar[random(0, 25)]);
+  
+  idInput = str + str2;
+  Serial.print("ID Input: ");
+  Serial.println(idInput);
+}
+
+
+void sendFirstMessage(){
+  String data = "{\"iDis\":\"";
+    data += idDispositivo;
+    data += "\",\"iOut-1\":\"";
+    data += idOutput1;
+    data += "\",\"desOut-1\":\"Luces\"";
+    data += ",\"iOut-2\":\"";
+    data += idOutput2;
+    data += "\",\"desOut-2\":\"Agua\"";
+    data += ",\"iIn-1\":\"";
+    data += idInput;
+    data += "\",\"desIn-1\":\"Temp\"";
+    data += "}";
+
+    data.toCharArray(msg, 150);
+    mqtt_client.publish(getTopic("response"), msg);  
+}
+
+void receiveJson(String str){
+  String receiveId = getValue(str,',',0);
+
+  if(receiveId == idOutput1){
+//    Serial.println("ES OUTPUT1");
+    String receivedText = getValue(str, ',', 2);
+//    Serial.println(receivedText);
+    String receivedStatus = getValue(str, ',', 3);
+//    Serial.println(receivedStatus);
+    if(receivedText == "ON"){
+      digitalWrite(LED_PIN, HIGH);
+//      Serial.println("SE ENCENDIO");
+    }else if(receivedText == "OFF"){
+      digitalWrite(LED_PIN, LOW);
+//      Serial.println("SE APAGO");
+    }
+  }else if(receiveId == idOutput2){
+//    Serial.println("ES OUTPUT2");
+    String receivedText = getValue(str, ',', 2);
+//    Serial.println(receivedText);
+    String receivedStatus = getValue(str, ',', 3);
+//    Serial.println(receivedStatus);
+    if(receivedText == "ON"){
+      digitalWrite(BOMB_PIN, HIGH);
+//      Serial.println("SE ENCENDIO");
+    }else if(receivedText == "OFF"){
+      digitalWrite(BOMB_PIN, LOW);
+//      Serial.println("SE APAGO");
+    }
+  }else if(receiveId == idInput){
+//    Serial.println("ES INPUT");
+    String receivedFreq = getValue(str, ',', 3);
+    PUBLISH_PERIOD = receivedFreq.toInt();
+//    Serial.print("FRECUENCIA: ");
+//    Serial.println(PUBLISH_PERIOD);
+
+  }
+}
+
+//Obtenemos valor puro
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
 
 void setupTempSensor() {
   Serial.println("Setting up BME280 Sensor");
@@ -123,11 +260,15 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
 
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  pinMode(BOMB_PIN, OUTPUT);
+  digitalWrite(BOMB_PIN, LOW);
 
-  pinMode(buttonPin, INPUT);
+  //Generate Unique Ids
+//  generateIdDispositivo();
+//  generateIdOutput1();
+//  generateIdOutput2();
+//  generateIdInput();
+
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -141,43 +282,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("'");
   Serial.print(msg_r);
   Serial.println("'");
-  if (strcmp("ON", msg_r) == 0) {
-    Serial.println("LED ON");
-    digitalWrite(LED_PIN, HIGH);
-  } else if(strcmp("OFF", msg_r) == 0) {
-    Serial.println("LED OFF");
-    digitalWrite(LED_PIN, LOW);
-  }else{
-//    freq = atoi(msg_r);
-  Serial.print("RECIBIDO: ");
-  Serial.println(msg_r);
-  PUBLISH_PERIOD = atoi(msg_r);
-  }
+  receiveJson(String(msg_r));
+//  if (strcmp("ON", msg_r) == 0) {
+//    Serial.println("LED ON");
+//    digitalWrite(LED_PIN, HIGH);
+//  } else if(strcmp("OFF", msg_r) == 0) {
+//    Serial.println("LED OFF");
+//    digitalWrite(LED_PIN, LOW);
+//  }else{
+//    Serial.print("RECIBIDO: ");
+//    Serial.println(msg_r);
+//    PUBLISH_PERIOD = atoi(msg_r);
+//  }
 }
 
-void colorConverter(String hexValue)
-{
-    int number = (int) strtol( &hexValue[1], NULL, 16);
-    int r = number >> 16;
-    int g = number >> 8 & 0xFF;
-    int b = number & 0xFF;
 
-    Serial.print("red is ");
-    Serial.println(r);
-    Serial.print("green is ");
-    Serial.println(g);
-    Serial.print("blue is ");
-    Serial.println(b);
-    setColor((r+50), g, b);
 
-}
-
-void setColor(int red, int green, int blue)
-{
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);  
-}
 void reconnect() {
   // Loop until we're reconnected
   while (!mqtt_client.connected()) {
@@ -186,7 +306,7 @@ void reconnect() {
     if (mqtt_client.connect(TEAM_NAME)) {
       Serial.println("connected");
       mqtt_client.subscribe(getTopic("response"));
-      mqtt_client.subscribe(getTopic("rgb"));
+//      sendFirstMessage();
     }else {
       Serial.print("failed, rc=");
       Serial.print(mqtt_client.state());
@@ -203,7 +323,7 @@ void publish(char* topic, char* payload) {
 }
 
 char* getTopic(char* topic) {
-  sprintf(topic_name, "/%s/%s", TEAM_NAME, topic);
+  sprintf(topic_name, "/%s", topic);
   return topic_name;
 }
 
@@ -232,7 +352,7 @@ void loop() {
     data += str;
     data += "\"}";
 
-    data.toCharArray(msg, 50);
+    data.toCharArray(msg, 150);
     mqtt_client.publish(getTopic("devices"), msg);     
     delay(5000);
   }
